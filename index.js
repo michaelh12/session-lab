fd8140553cb9846c6f659fd3c58bbb14113b532a
Michael Habinsky <mhabinsky@live.com>
2018-12-11 23:12:15 -0500

    completed

---
+++ index.js
@@ -0,0 +1,32 @@
+//index.js
+
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(
+  session({
+    secret: 'SunnyB3aches',
+
+    resave: false,
+
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log('session', req.session);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.count);
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  res.send('Hello');
+});
+
+app.listen(8080, () => console.log('Listening at http://localhost:8080'));
